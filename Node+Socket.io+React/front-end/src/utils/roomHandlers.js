457dbc46970d1beccdb88cedfb75a1e4fda362b0
David <davidbish2002@hotmail.com>
2023-12-22 22:52:10 -0330

    roomHandlers.js was unfinished and should of been committed, now fixed.

--- Node+Socket.io+React/front-end/src/utils/roomHandlers.js
+++ Node+Socket.io+React/front-end/src/utils/roomHandlers.js
@@ -1,9 +1,11 @@
 import { store } from "../redux/store";
+import roomConnect from "../services/roomConnect";
 import history from "./history";
 
 export const handleJoinRoom = (room) => {
+  console.log("Hello?");
   const roomId = room.toLowerCase();
-  dispatch(roomConnect({ type: "join", roomId }));
+  store.dispatch(roomConnect({ type: "join", roomId }));
   history.push(`/rooms/${roomId}`);
 };
 
