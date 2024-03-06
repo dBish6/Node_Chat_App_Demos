9bfc3fba863ff1a08fca60905f18463090d22237
David <davidbish2002@hotmail.com>
2024-03-05 23:49:55 -0330

    Removed usePolling.

--- Node+Socket.io+React/front-end/vite.config.js
+++ Node+Socket.io+React/front-end/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { port: 3000, watch: { usePolling: true } },
+  server: { port: 3000 },
   plugins: [react()],
   build: { outDir: "build" },
 });
